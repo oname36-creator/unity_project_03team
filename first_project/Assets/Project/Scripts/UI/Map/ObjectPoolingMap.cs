4fc73a034c04796bfa5dbbca2ed8b3df2ed3c556
DESKTOP-CP9D9T5\inha <rmaapekf2086@naver.com>
2026-07-01 20:21:34 +0900

    Feat : 맵 기믹 추가
    - 4종류의 맵 기믹 추가

---
+++ first_project/Assets/Project/Scripts/UI/Map/ObjectPoolingMap.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ObjectPoolingMap : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 맵 기믹 프리팹들 생성
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 배경이 지나갈 때 마다 맵 기믹 갱신
+        // 순서는 중복없는 랜덤, 정해진 기믹을 다 돌면
+        // MapChenk_A를 제외한 나머지 기믹에서 다시 랜덤으로 돈다.
+    }
+}
