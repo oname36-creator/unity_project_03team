ed65af90478922e0660999ee6744aadf7d2f6264
BFandLee <vactor224@gmail.com>
2026-07-08 23:49:36 +0900

    Refactor : 맵 데이터 전송(2)
    - 맵이 오른쪽으로 자연스럽게 나오도록 버그픽스 중
    
    Fixes : #11

--- first_project/Assets/Project/Scripts/UI/Map/MapManager.cs
+++ first_project/Assets/Project/Scripts/UI/Map/MapManager.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.PlayerLoop;
+using System.Collections;
 
+// 페이즈별로 시간을 설정하기 위한 구조체
+[System.Serializable]
+public struct PhaseData
+{
+    public string phaseName;
+    [Tooltip("한 페이즈가 끝나는 시간")]
+    public float timeThreshold;
+    [Tooltip("해당 페이즈에 등장할 맵들이 들어있는 SO")]
+    public SOMapPalette phasePalette;
+}
 public class MapManager : MonoBehaviour
 {
-    [Header("맵 데이터")]
-    public SOMapPalette currentMapData;
-
-    [Header("세팅")]
+    [Header("시작 설정")]
+    public GameObject safeZonePrefab;
     public Transform Player;
     public float spawnTriggerDistance = 30f;
+
+    [Header("난이도 페이즈 설정")]
+    public List<PhaseData> phases;
+    private int currentPhaseIndex = 0;
+    private float gameTimer = 0f;
+   
 
     // 프리팹 종류별로 관리
     private Dictionary<GameObject, Queue<GameObject>> mapPools = new Dictionary<GameObject, Queue<GameObject>>();
@@ -22,25 +38,33 @@ public class MapManager : MonoBehaviour
     private Vector3 nextSpawnPosition = Vector3.zero;
     void Start()
     {
-        if (currentMapData == null || currentMapData.chunkPrefabs.Count == 0)
-        {
-            Debug.LogError("MapManager에 SO 데이터가 없거나 비어있다!");
-            return;
-        }
+        InitializationPools();
 
-        // 1. 게임 시작 시 각 프리팹마다 풀 바구니 생성 및 셔블 백 초기화
-        foreach (var prefab in currentMapData.chunkPrefabs)
-        {
-            mapPools.Add(prefab, new Queue<GameObject>());
-        }
+        // 시작 위치 0으로 고정
+        Vector3 spawnPos = Vector3.zero;
 
-        // 2. 초기 맵 4개 세팅
-        for (int i = 0; i < 4; ++i)
-        {
-            SpawnNextMap();
-        }
+        // 안전지대 생성
+        SpawnSpecificMap(safeZonePrefab);
 
+        UpdateShuffleBagForCurrentPhase();
+
+        spawnPos = SpawnMap(GetNextPrefabFromShuffleBag(), spawnPos);
+
+        nextSpawnPosition = spawnPos;
+        // 1페이즈 맵들로 셔플 백 채우기
+        StartCoroutine(PhaseTimerRoutine());
     }
+
+    void Update()
+    {
+        if(Player.position.x + spawnTriggerDistance > nextSpawnPosition.x)
+        {
+            nextSpawnPosition = SpawnMap(GetNextPrefabFromShuffleBag(), nextSpawnPosition);
+            RecycleOldMap();
+        }
+    }
+
+    
 
     private void SpawnNextMap()
     {
@@ -85,7 +109,7 @@ public class MapManager : MonoBehaviour
         oldMap.SetActive(false);
 
         // 자신이 있던 pool의 위치 찾아서 다시 돌아감
-        foreach (var prefab in currentMapData.chunkPrefabs)
+        foreach (var prefab in phases[currentPhaseIndex].phasePalette.chunkPrefabs)
         {
             if (oldMap.name == prefab.name)
             {
@@ -95,13 +119,61 @@ public class MapManager : MonoBehaviour
         }
     }
 
+    private void InitializationPools()
+    {
+        // 안전지대 바구니 생성
+        mapPools.Add(safeZonePrefab, new Queue<GameObject>());
+        // 모든 페이즈 맵들을 미리 세팅
+        foreach(var phase in phases)
+        {
+            foreach(var prefab in phase.phasePalette.chunkPrefabs)
+            {
+                // 중복 방지
+                if(!mapPools.ContainsKey(prefab))
+                {
+                    mapPools.Add(prefab, new Queue<GameObject>());
+                }
+            }
+        }
+    }
+
+    // 특정 맵 강제 스폰
+    private void SpawnSpecificMap(GameObject prefab)
+    {
+        GameObject mapToSpawn;
+        if (mapPools[prefab].Count > 0)
+        {
+            mapToSpawn = mapPools[prefab].Dequeue();
+            mapToSpawn.SetActive(true);
+        }
+        else 
+        {
+            mapToSpawn = Instantiate(prefab);
+            mapToSpawn.name = prefab.name;
+        }
+
+        mapToSpawn.transform.position = nextSpawnPosition;
+        activeMaps.Enqueue(mapToSpawn);
+
+        // Find 쓰지말랬는데
+        Transform endPos = mapToSpawn.transform.Find("EndPosition");
+        nextSpawnPosition = endPos != null ? endPos.position : nextSpawnPosition;   // 이것도 간단한 문법있지 않나?
+    }
+
+    private void UpdateShuffleBagForCurrentPhase()
+    {
+        // 이전 페이즈 맵 정리 후 현재 페이즈 맵으로 교체
+        shuffleBag.Clear();
+        shuffleBag.AddRange(phases[currentPhaseIndex].phasePalette.chunkPrefabs);
+    }
+
     // 중복 없이 랜덤 추출
     private GameObject GetNextPrefabFromShuffleBag()
     {
         // pool에 값 없으면 다시 채워 넣음
         if(shuffleBag.Count ==0)
         {
-            shuffleBag.AddRange(currentMapData.chunkPrefabs);
+            shuffleBag.AddRange(phases[currentPhaseIndex].phasePalette.chunkPrefabs);
         }
 
         // 랜덤 뽑기
@@ -112,5 +184,61 @@ public class MapManager : MonoBehaviour
         shuffleBag.RemoveAt(randomIndex);
 
         return pickedPrefab;
+    }
+
+    // 스폰하고 나서 다음 스폰 위치를 반환하는 함수
+    private Vector3 SpawnMap(GameObject prefab, Vector3 spawnPos)
+    {
+        GameObject map = null;
+
+        if(mapPools.ContainsKey(prefab) && mapPools[prefab].Count > 0)
+        {
+            map = mapPools[prefab].Dequeue();
+            map.SetActive(true);
+        }
+        else
+        {
+            map = Instantiate(prefab);
+            map.name = prefab.name;
+        }
+
+        map.transform.position = spawnPos;
+        activeMaps.Enqueue(map);
+
+        // EndPosition 찾기
+        Transform endPos = map.transform.Find("EndPosition");
+        if (endPos != null)
+        {
+            return endPos.position;
+        }
+        else
+        {
+            Debug.LogError($"{prefab.name}에 EndPosition이 없습니다");
+            return spawnPos;
+        }
+    }
+
+    private IEnumerator PhaseTimerRoutine()
+    {
+        for(currentPhaseIndex = 0; currentPhaseIndex < phases.Count; currentPhaseIndex++)
+        {
+            PhaseData currentPhase = phases[currentPhaseIndex];
+
+            // 테스트용 디버깅
+            Debug.Log($"[{currentPhase.phaseName}] 돌입!, 맵 세팅 변경됨");
+
+            // 현재 페이즈의 맵들로 셔플 백 갈아끼우기
+            UpdateShuffleBagForCurrentPhase();
+
+            // 마지막 페이즈 -> 무한 대기(영원히 지속됨)
+            if(currentPhaseIndex < phases.Count-1)
+            {
+                yield return new WaitForSeconds(currentPhase.timeThreshold);
+            }
+            else 
+            {
+                yield break;
+            }
+        }
     }
 }
