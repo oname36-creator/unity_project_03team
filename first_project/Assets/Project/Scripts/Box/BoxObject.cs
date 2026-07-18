6887ba0178d16fb4c8cd3fc6ea4bb90ed98b1ded
DESKTOP-CP9D9T5\inha <vactor224@gmail.com>
2026-07-18 17:11:18 +0900

    Refactor : 상자 스폰 및 아이템 생성(2)
    - 버그 픽스 : 상자에서 아이템 안나옴 ->  콜라이더에서 is Trigger을 켜서 해결
    - 플레이어가 1개의 상자에서 2개의 아이템을 먹음 --> 상자와 플레이어의 충돌이 한번 일어나면 상자의 콜라이더를 비활성화하여 중복을 허용하지 않게끔 처리

--- first_project/Assets/Project/Scripts/Box/BoxObject.cs
+++ first_project/Assets/Project/Scripts/Box/BoxObject.cs
@@ -2,12 +2,27 @@ using UnityEngine;
 
 public class BoxObject : MonoBehaviour
 {
-    private ItemData[] rewardItems;
+    private ItemData[] _rewardItems;
+    private Collider2D _triggerCollider;
+    private bool _isTriggered = false;
 
+    private void Awake()
+    {
+        _triggerCollider = GetComponent<Collider2D>();
+    }
+
+    private void OnEnable()
+    {
+        _isTriggered = false;
+        if(_triggerCollider)
+        {
+            _triggerCollider.enabled = true;
+        }
+    }
     // 스폰 지점에 보상 아이템 목록을 주입받음
     public void SetRewardItems(ItemData[] Items)
     {
-        rewardItems = Items;
+        _rewardItems = Items;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -15,8 +30,16 @@ public class BoxObject : MonoBehaviour
         // 디버깅용 로그: 어떤 오브젝트와 충돌했는지 콘솔에 출력합니다.
         Debug.Log($"[BoxObject] 충돌 감지됨: {collision.gameObject.name}, Tag: {collision.gameObject.tag}");
 
+        // 중복 실행 방지를 위해 리턴
+        if (_isTriggered) return;
         if(collision.CompareTag("Player"))
         {
+            // 플레이어 접촉 즉시 콜라이더를 비활성화
+            _isTriggered = true;
+            if(_triggerCollider != null)
+            {
+                _triggerCollider.enabled = false;
+            }
             Debug.Log("[BoxObject] 플레이어 충돌 확인 -> 상자 열기 실행");
             OpenBox();
         }
@@ -25,10 +48,10 @@ public class BoxObject : MonoBehaviour
     private void OpenBox()
     {
         #region AddItem
-        if(rewardItems != null && rewardItems.Length > 0)
+        if(_rewardItems != null && _rewardItems.Length > 0)
         {
             // 보상 목록 중 무작위로 아이템 1개 선택
-            ItemData selectedItem = rewardItems[Random.Range(0, rewardItems.Length)];
+            ItemData selectedItem = _rewardItems[Random.Range(0, _rewardItems.Length)];
             if(selectedItem != null)
             {
                 // DataManager에 아이템 추가
